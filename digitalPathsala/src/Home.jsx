f50b0086cc49bb873f06635401e121829a35e5e0
Saroj Maharjan <maharjan.saroj93@gmail.com>
2025-05-14 11:15:00 +0545

    update

--- digitalPathsala/src/Home.jsx
+++ digitalPathsala/src/Home.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [pi, setPi] = useState(3.14);
 
-    const [pi, setPi] = useState(3.14); 
+  let [age, setAge] = useState(32);
 
-    let [age, setAge] = useState(32);
+  const [joke, setJoke] = useState("");
 
-    function increaseAge(){
-        setAge(age + 1);
-        return age;
-    }   
+  useEffect(() => {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((serverkodata) => serverkodata.json())
+      .then((actualjoke) =>
+        setJoke(`${actualjoke.setup} : ${actualjoke.punchline}`)
+      );
+  }, []);
 
-    function decreaseAge(){
-        setAge(age - 1);
-        return age;
-    }
+  function increaseAge() {
+    setAge(age + 1);
+    return age;
+  }
+
+  function decreaseAge() {
+    setAge(age - 1);
+    return age;
+  }
 
   return (
     <div>
-        <h1>{}</h1>
-        <button>+</button>
-        <button>-</button>
+      <h1>{joke}</h1>
+      <button>+</button>
+      <button>-</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
